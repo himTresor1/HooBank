5e28394d07b570de25493e9735c8cdc059582c54
himTresor1 <amizeropeace@gmail.com>
2022-10-17 11:15:18 +0200

    NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,14 @@ import {navLinks} from "../constants"
 
 const Navbar = () => {
   return (
-    <div className='w-full flex py-6 justify-between items-center navbar'>
-      
-    </div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt="hoobank" className='w-[124px] h-[32px]' />
+      <ul className='list-none sm:flex hidden justfy-center items-center flex-1'>
+        {navLinks.map((nav,index)=>(
+          <li key={nav.id} className={`font-poppins`}></li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
