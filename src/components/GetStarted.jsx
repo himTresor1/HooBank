784963873bdf3288684c6023835450997bb95230
himTresor1 <amizeropeace@gmail.com>
2022-10-19 08:08:30 +0200

    Herooo

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,13 +4,13 @@ import { arrowUp } from '../assets/assets'
 
 const GetStarted = () => (
     <div className={` ${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer` }>
-        <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%]`}>
-            <div className={` ${styles.flexStart} flex-row `}>
-                <p className='font-poppins foint-medium text-[18px] leading-[23px]'>
+        <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+            <div className={` ${styles.flexStart} flex-col `}>
+                <p className='font-poppins foint-medium text-[18px]  flex flex-row mr-2 leading-[23px]'>
                     <span className='text-gradient'>
                         Get 
                     </span>
-                    <img src={arrowUp} alt="arrow"  className='w-[32px] h-[32px] object-contain '/>
+                    <img src={arrowUp} alt="arrow"  className='w-[28px] h-[28px] object-contain '/>
                 </p>
                 <p className='font-poppins foint-medium text-[18px] leading-[23px]'>
                     <span className='text-gradient'>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import GetStarted from "./GetStarted";
 const Hero = () => (
   <section
     id="home"
-    className={` flex md:flex-row flex-col  ${styles.paddingY}`}
+    className={` flex md:flex-row flex-col  w-full ${styles.paddingY}`}
   >
     <div
       className={`flex-1  ${styles.flexStart}  flex-col xl:px-0 sm:px-16 px-8 mx-10`}
@@ -24,15 +24,29 @@ const Hero = () => (
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
           The next
           <br className="sm:block hidden" /> {"   "}
-          <span className="text-gradient">Generation</span>   <br /> {"  "} 
+          <span className="text-gradient">Generation</span>{"  "} 
         
-          Payment Method.
+         
         </h1>
         <div className="ss:flex hidden md:mr-4 mr-0">
           <GetStarted/>
         </div>
       </div>
+      <h1  className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full ">
+          Payment method
+      </h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5 `}>Our team aof experts uses a methodology to identify the credit cards most likely to fit your needs.We examine annual percentage rates , annual fees.
+
+      </p>
     </div>
+    <div className="">
+      <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+      <div className="absolute z-[0] x-[40%] h-[35%] top-0 pink__gradient "></div>
+      <div className="absolute z-[1] x-[80%] h-[75%] rounded-full  bottom-40 white__gradient "></div>
+      <div className="absolute z-[0] x-[50%] h-[55%] rounded-full   right-20 bottom-20 blue__gradient "></div>
+    </div>
+   
+    
   </section>
 );
 
