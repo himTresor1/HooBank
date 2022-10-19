e7672b727a9fed537a5e089fd88ce158f215a30e
himTresor1 <amizeropeace@gmail.com>
2022-10-19 06:22:41 +0200

    Started the Hero Section

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -5,7 +5,7 @@ const styles = {
     paragraph: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]",
   
     flexCenter: "flex justify-center items-center",
-    flexStart: "flex justify-center items-start",
+    flexStart: "flex justify-start items-start",
   
     paddingX: "sm:px-16 px-6",
     paddingY: "sm:py-16 py-6",
