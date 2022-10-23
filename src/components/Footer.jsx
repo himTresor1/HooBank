d07e8d890736514d5a8ae535b931f973c61d689e
himTresor1 <amizeropeace@gmail.com>
2022-10-23 18:37:58 +0200

    Fixed the footer's social media icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,15 +32,15 @@ const Footer = () => (
             ))}
            </div>
      </div>
-    <div className="w-full flex-justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[3f3r45] ">
+    <div className="w-full justify-between items-center flex md:flex-row flex-row pt-6 border-t-[1px] border-t-[3f3r45] ">
       <p  className=' font-poppins  font-normal text-center text-[18px] leading-[27px] text-white '>
-        2021 HooBank. ALl right Reserved.
+        2021 HooBank. All right Reserved.
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
          {socialMedia.map((social , index)=>(
            <img
-           src={social.icon}  alt={social.id} key={social.id} className={`w-[210px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length-1 ? 'mr-6' : 'mr-0'}`}/>
+           src={social.icon}  alt={social.id} key={social.id} className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length-1 ? 'mr-6' : 'mr-0'}`}/>
          ))}
       </div>
     </div>
