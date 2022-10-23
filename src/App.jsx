4aacfebff708080604e890a8dbdf4272d263a4ce
himTresor1 <amizeropeace@gmail.com>
2022-10-23 18:52:14 +0200

    Finished the project

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => (
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
+    <div className={`bg-primary ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Hero />
       </div>
